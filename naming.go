6b814d462dcfe34ecbb11c345518f5131bb5968a
Muhammad Iskandar Dzulqornain <midzulqornain@gmail.com>
2020-04-23 14:52:45 +0700

    add functionality for listAll, rollback, startIncrement, formatIncrement

--- naming.go
+++ naming.go
@@ -10,10 +10,16 @@ import (
 )
 
 const (
-	formulaOption      = "formula"
-	pathOption         = "path"
-	isFileOnlyOption   = "fileOnly"
-	isFolderOnlyOption = "folderOnly"
+	formulaOption            = "formula"
+	ignoreOption             = "ignore"
+	pathOption               = "path"
+	rollbackOption           = "rollback"
+	startOption              = "start"
+	isFileOnlyOption         = "fileOnly"
+	isFolderOnlyOption       = "folderOnly"
+	isListAllOption          = "listAll"
+	isSkipErrorOption        = "skipError"
+	formattedIncrementOption = "formatIncrement"
 
 	namingTypeFile   = "file_only"
 	namingTypeFolder = "folder_only"
@@ -21,81 +27,191 @@ const (
 
 	formulaIncrement = "{increment}"
 	formulaCurrent   = "{current}"
+	formulaSlash     = "{slash}"
 
-	defaultIncrement = 1
+	defaultIncrement          = 1
+	defaultFormattedIncrement = 4
 )
 
-var formulas = []string{formulaIncrement, formulaCurrent}
+var formulas = []string{formulaIncrement, formulaCurrent, formulaSlash}
 
 func main() {
 	fmt.Println("Naming your files & folders quickly\n")
+	var increment int
 
-	path, formula, namingType, err := readArgs()
+	path, formula, namingType, isListAll, isRollback, isSkipError, ignoreList, startIncrement, formattedIncrement, err := readArgs()
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := exec(formula, path, defaultIncrement, namingType); err != nil {
+	if isListAll != nil && *isListAll {
+		fmt.Println("List All files & folders in tree")
+		if err = list(path); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	if isRollback != nil && *isRollback {
+		fmt.Println("Rollback files")
+		increment, err = execRollback(formula, path, startIncrement, namingType, ignoreList)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Successfully rollback %d items for %s", increment-1, namingType)
+		return
+	}
+
+	increment, err = exec(formula, path, startIncrement, namingType, ignoreList, *isSkipError, formattedIncrement)
+	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println("Successfully naming files & folders")
+	fmt.Printf("Successfully naming %d items for %s", increment-1, namingType)
 }
 
-func exec(formula, path string, increment int, namingType string) error {
+func exec(formula, path string, increment int, namingType string, ignoreList []string, isSkipError bool, formattedIncrement int) (int, error) {
 	files, folders, err := readAll(path)
 	if err != nil {
-		return err
+		return 0, err
 	}
 	prefixPath := path + "/"
 
 	if namingType == namingTypeFile || namingType == namingTypeAll {
-		if increment, err = naming(files, increment, formula, path); err != nil {
-			return err
+		if increment, err = naming(files, increment, formula, path, ignoreList, isSkipError, formattedIncrement); err != nil {
+			return 0, err
 		}
 	}
 
 	if namingType == namingTypeFolder || namingType == namingTypeAll {
-		if increment, err = naming(folders, increment, formula, path); err != nil {
-			return err
+		if increment, err = naming(folders, increment, formula, path, ignoreList, isSkipError, formattedIncrement); err != nil {
+			return 0, err
 		}
-		_, folders, err = readAll(path)
+		if _, folders, err = readAll(path); err != nil {
+			return 0, err
+		}
 	}
 
 	for _, folder := range folders {
-		if err = exec(formula, prefixPath+folder, increment+1, namingType); err != nil {
-			return err
+		if increment, err = exec(formula, prefixPath+folder, increment, namingType, ignoreList, isSkipError, formattedIncrement); err != nil {
+			return 0, err
 		}
 	}
 
-	return nil
+	return increment, nil
 }
 
-func naming(src []string, increment int, formula, path string) (lastIncrement int, err error) {
+func execRollback(formula, path string, increment int, namingType string, ignoreList []string) (int, error) {
+	files, folders, err := readAll(path)
+	if err != nil {
+		return 0, err
+	}
+	prefixPath := path + "/"
+
+	if namingType == namingTypeFile || namingType == namingTypeAll {
+		if increment, err = rollback(files, increment, formula, path, ignoreList); err != nil {
+			return 0, err
+		}
+	}
+
+	if namingType == namingTypeFolder || namingType == namingTypeAll {
+		if increment, err = rollback(folders, increment, formula, path, ignoreList); err != nil {
+			return 0, err
+		}
+		if _, folders, err = readAll(path); err != nil {
+			return 0, err
+		}
+	}
+
+	for _, folder := range folders {
+		if increment, err = execRollback(formula, prefixPath+folder, increment, namingType, ignoreList); err != nil {
+			return 0, err
+		}
+	}
+
+	return increment, nil
+}
+
+func list(path string) (err error) {
+	var files, folders []string
+	if files, folders, err = readAll(path); err != nil {
+		return
+	}
+	prefixPath := path + "/"
+	for _, file := range files {
+		fmt.Println(prefixPath + file)
+	}
+	for _, folder := range folders {
+		if err = list(prefixPath + folder); err != nil {
+			return
+		}
+	}
+	return
+}
+
+func naming(src []string, increment int, formula, path string, ignoreList []string, isSkipError bool, formattedIncrement int) (lastIncrement int, err error) {
 	prefixPath := path + "/"
 	for _, s := range src {
-		increment++
-		destName := readFormula(formula, s, increment)
-		if err = rename(prefixPath+s, prefixPath+destName); err != nil {
-			return
+		destName := readFormula(formula, s, increment, formattedIncrement)
+		if okToRename(s, ignoreList) {
+			if err = rename(prefixPath+s, prefixPath+destName); err == nil {
+				increment++
+			}
+			if err != nil && !isSkipError {
+				return
+			}
 		}
 	}
 	lastIncrement = increment
 	return
 }
 
-func readArgs() (pathDir, formulaNaming, namingType string, err error) {
-	formula := flag.String(formulaOption, "", `[mandatory] formula for naming files/folders. i.e: --formula="finance{increment} - {currentName} | output: finance1 - How to manage money"`)
-	path := flag.String(pathOption, "./", "[optional] read path")
+func rollback(src []string, increment int, formula, path string, ignoreList []string) (lastIncrement int, err error) {
+	prefixPath := path + "/"
+	for _, s := range src {
+		destName := readFormulaRollback(formula, s, increment)
+		if okToRename(s, ignoreList) {
+			if err = rename(prefixPath+s, prefixPath+destName); err != nil {
+				return
+			}
+			increment++
+		}
+	}
+	lastIncrement = increment
+	return
+}
+
+func readArgs() (pathDir, formulaNaming, namingType string, isListAll, isRollback, isSkipError *bool, ignoreList []string, startIncrement, formattedIncrement int, err error) {
+	formula := flag.String(formulaOption, "", `[mandatory] formula for naming files/folders. i.e: --formula="A{slash}B{slash}finance{increment} - {current} | output: A/B/finance1 - How to manage money"`)
+	path := flag.String(pathOption, ".", "[optional] read path")
+	start := flag.Int(startOption, defaultIncrement, "[optional] start increment value")
+	formattedIncr := flag.Int(formattedIncrementOption, defaultFormattedIncrement, "[optional] increment format. i.e: 4. it will print increment like this format 0001, 0002, 0003")
+	isRollback = flag.Bool(rollbackOption, false, "[optional] for rollback only")
 	isFileOnly := flag.Bool(isFileOnlyOption, false, "[optional] naming for files only")
 	isFolderOnly := flag.Bool(isFolderOnlyOption, false, "[optional] naming for folders only")
+	isListAll = flag.Bool(isListAllOption, false, "[optional] for listing all files and folders")
+	isSkipError = flag.Bool(isSkipErrorOption, false, "[optional] ignore error whenever it's failed to rename one of the file")
+	ignores := flag.String(ignoreOption, "", `[optional] ignore list, comma-separated. i.e --ignore="Desktop.ini,*.exe" | * means all`)
+
 	flag.Parse()
+	ignoreList = strings.Split(*ignores, ",")
+
+	if len(ignoreList) == 0 && *ignores != "" {
+		ignoreList = append(ignoreList, *ignores)
+	}
+	pathDir = *path
+	namingType = getNamingType(*isFileOnly, *isFolderOnly)
+
+	if isListAll != nil && *isListAll {
+		return
+	}
+
 	if formula == nil || *formula == "" {
 		err = fmt.Errorf("%s is Mandatory", formulaOption)
 		return
 	}
 	formulaNaming = *formula
-	pathDir = *path
-	namingType = getNamingType(*isFileOnly, *isFolderOnly)
+	startIncrement = *start
+	formattedIncrement = *formattedIncr
 	return
 }
 
@@ -104,7 +220,7 @@ func getNamingType(isFileOnly, isFolderOnly bool) string {
 		return namingTypeFile
 	}
 	if isFolderOnly {
-		return namingTypeFile
+		return namingTypeFolder
 	}
 	return namingTypeAll
 }
@@ -140,12 +256,114 @@ func rename(src, dst string) error {
 	return nil
 }
 
-func readFormula(in, currentName string, index int) string {
+// format i.e: 4
+func getFormattedInt(format int, val int) string {
+	stringFormat := "%" + fmt.Sprint(format) + "d"
+	out := fmt.Sprintf(stringFormat, val)
+	out = strings.ReplaceAll(out, " ", "0")
+	return out
+}
+
+func readFormula(in, currentName string, index, formattedIncrement int) string {
 	if strings.Contains(in, formulaIncrement) {
-		in = strings.ReplaceAll(in, formulaIncrement, fmt.Sprint(index))
+		in = strings.ReplaceAll(in, formulaIncrement, getFormattedInt(formattedIncrement, index))
 	}
 	if strings.Contains(in, formulaCurrent) {
 		in = strings.ReplaceAll(in, formulaCurrent, currentName)
 	}
+	if strings.Contains(in, formulaSlash) {
+		in = strings.ReplaceAll(in, formulaSlash, "/")
+	}
 	return in
+}
+
+func readFormulaRollback(formula, currentName string, index int) string {
+	increment := getIncrementString(formula, currentName)
+	formula = strings.ReplaceAll(formula, formulaIncrement, increment)
+	realFilename := getRealFilename(formula, currentName)
+	return realFilename
+}
+
+func getIncrementString(formula, filename string) string {
+	if !strings.Contains(formula, formulaIncrement) {
+		return ""
+	}
+	formula = strings.Replace(formula, formulaCurrent, "", -1)
+	return stringBetween(filename, stringBefore(formula, formulaIncrement), stringAfter(formula, formulaIncrement))
+}
+
+func getRealFilename(formula, filename string) string {
+	if !strings.Contains(formula, formulaCurrent) {
+		return ""
+	}
+	formula = strings.Replace(formula, formulaIncrement, "", -1)
+	before := stringBefore(formula, formulaCurrent)
+	after := stringAfter(formula, formulaCurrent)
+	filename = strings.Replace(filename, before, "", 1)
+	filename = strings.Replace(filename, after, "", 1)
+	return filename
+}
+
+func stringSliceContains(in []string, v string) bool {
+	for _, s := range in {
+		if s == v {
+			return true
+		}
+	}
+	return false
+}
+
+func okToRename(v string, ignoreList []string) bool {
+	if stringSliceContains(ignoreList, v) {
+		return false
+	}
+	for _, ignore := range ignoreList {
+		if strings.Contains(ignore, "*.") {
+			splitted := strings.Split(ignore, "*.")
+			ignoreExtension := splitted[len(splitted)-1]
+			if extension, err := getFileExtension(v); err == nil {
+				if extension == ignoreExtension {
+					return false
+				}
+			}
+		}
+	}
+	return true
+}
+
+func getFileExtension(fileName string) (string, error) {
+	splitted := strings.Split(fileName, ".")
+	if len(splitted) > 0 {
+		extension := splitted[len(splitted)-1]
+		return extension, nil
+	}
+	return "", fmt.Errorf("No file extension!")
+}
+
+func stringBetween(value string, a string, b string) string {
+	if !strings.Contains(value, a) || !strings.Contains(value, b) {
+		return ""
+	}
+	value = strings.Replace(value, a, "", 1)
+	return stringBefore(value, b)
+}
+
+func stringAfter(value string, a string) string {
+	pos := strings.LastIndex(value, a)
+	if pos == -1 {
+		return ""
+	}
+	adjustedPos := pos + len(a)
+	if adjustedPos >= len(value) {
+		return ""
+	}
+	return value[adjustedPos:len(value)]
+}
+
+func stringBefore(value string, a string) (out string) {
+	slices := strings.Split(value, a)
+	if len(slices) < 2 {
+		return
+	}
+	return slices[0]
 }
